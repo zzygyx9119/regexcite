7eb1604a166b114068b5905315e4aa3f46479249
zuyu zheng <zhengzuyu@gmail.com>
2025-04-14 10:08:12 -0700

    first commit

--- R/strsplit1.R
+++
@@ -1,8 +0,0 @@
-# (x <- "alfa,bravo,charlie,delta")
-# strsplit(x, split = ",")
-# unlist(strsplit(x, split = ","))
-# strsplit(x, split = ",")[[1]]
-
-strsplit1 <- function(x, split) {
-  strsplit(x, split = split)[[1]]
-}
